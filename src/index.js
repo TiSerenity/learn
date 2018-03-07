2435556cefeb6a32d16a66c61f047eefc647ac67
bidanjun <bidanjun@gmail.com>
2018-03-08 01:36:15 +0800

    add greeting example and doc

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+
+class App extends Component {
+  render() {
+    return (
+      <div>        
+          <h1>hello world</h1>
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+
